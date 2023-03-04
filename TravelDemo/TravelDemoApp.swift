65ffb72c72e9c1508aea9bdc5eb5e9a7d5ff058e
ThidarPhyo <thidarphyo22031996@gmail.com>
2023-03-04 19:58:56 +0900

    Initial Commit

---
+++ TravelDemo/TravelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDemoApp.swift
+//  TravelDemo
+//
+//  Created by cmStudent on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
